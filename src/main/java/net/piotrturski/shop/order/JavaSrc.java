482dffa2036fc32250aa69755f141f6b4f19a4c8
Piotr Turski <piotr.turski@gmail.com>
2019-01-06 23:02:20 +0100

    java 11

--- src/main/java/net/piotrturski/shop/order/JavaSrc.java
+++ src/main/java/net/piotrturski/shop/order/JavaSrc.java
@@ -1,10 +1,20 @@
 package net.piotrturski.shop.order;
 
+import org.springframework.lang.NonNull;
+
 import java.math.BigDecimal;
+import java.util.stream.Stream;
 
 public class JavaSrc {
 
     public static BigDecimal ZERO = BigDecimal.ZERO;
 
     public static Class c = ExceptionMapper.class;
+
+    public Stream<String> java11() {
+        var a = 7;
+        return Stream.of("")
+                .map((@NonNull var s) -> a + s.toLowerCase());
+
+    }
 }
